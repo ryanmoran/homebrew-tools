ae737913ce954b0a4a7d34a799483c6045ab8bdb
Ryan Moran <rmoran@pivotal.io>
2018-02-17 19:31:32 -0800

    Bumps to v0.7

--- Casks/Radar.rb
+++ Casks/Radar.rb
@@ -1,6 +1,6 @@
 cask "radar" do
-  version "0.6"
-  sha256 "bba5e0527ea735901aedae1ee22187e1712393e56eca14059412eb4215ee271d"
+  version "0.7"
+  sha256 "ad5242e8f082b8f1cb050332b3e4178a4464bc74d8f83c60c36ca4ae83708a8f"
 
   url "https://storage.googleapis.com/radar-builds/Radar-#{version}.tar.gz"
   name "Radar"
