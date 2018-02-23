4d6e5afe4089f72f5146b3f08274f3c710163e62
Ryan Moran <rmoran@pivotal.io>
2018-02-23 14:02:58 -0500

    Bumps radar to v0.10

--- Casks/radar.rb
+++ Casks/radar.rb
@@ -1,6 +1,6 @@
 cask "radar" do
-  version "0.9"
-  sha256 "ca220c64e968eb36d36bcd400a258acc99b965eaf2e1637df962cdc0c45de65d"
+  version "0.10"
+  sha256 "c5a30ed222a5db57cf71b50c738f268ce00b17c14557f2356c70ec7bd932d99b"
 
   url "https://storage.googleapis.com/radar-builds/Radar-#{version}.tar.gz"
   name "Radar"
