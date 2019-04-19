7f0c54fdf30381d047734d1467153ca2d1e7a099
Ryan Moran <rmoran@pivotal.io>
2019-04-19 10:06:53 -0400

    Bumps faux to v0.6.0

--- faux.rb
+++ faux.rb
@@ -1,14 +1,14 @@
 class Faux < Formula
   desc "Golang fake generator"
   homepage "https://github.com/ryanmoran/faux"
-  version "v0.5.0"
+  version "v0.6.0"
 
   if OS.mac?
     url "https://github.com/ryanmoran/faux/releases/download/#{version}/faux-darwin-amd64"
-    sha256 "5965bb22b0d4f635dd87df1df8173fcf4397085865e3db7544e96d7df94e3bfa"
+    sha256 "1c7e57cf8c0269421fe96df049160aa7ce558b3f4dd2ca8eb0ef1c89564a3115"
   elsif OS.linux?
     url "https://github.com/ryanmoran/faux/releases/download/#{version}/faux-linux-amd64"
-    sha256 "b484eb3e72f042a96a36dec694716969a4bda88450377d927872d7e849339895"
+    sha256 "49ef60ce7ff6ebe4eec1edd09b9b7360c382edd49c524bd528c5e3a411bd4944"
   end
 
   depends_on :arch => :x86_64
