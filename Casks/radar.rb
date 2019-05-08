f19c5f867393222f7cf76bc9dccaca5d01dd3ee1
Ryan Moran <rmoran@pivotal.io>
2019-05-07 20:53:17 -0700

    Moves radar releases bucket

--- Casks/radar.rb
+++ Casks/radar.rb
@@ -2,7 +2,7 @@ cask "radar" do
   version "0.20"
   sha256 "de8c42b0da0d73ce48d2c556a8d8006fb65c9f1f3a7feec98b613431397073be"
 
-  url "https://storage.googleapis.com/radar-builds/Radar-#{version}.tar.gz"
+  url "https://storage.googleapis.com/radar-releases/Radar-#{version}.tar.gz"
   name "Radar"
   homepage "https://github.com/ryanmoran/Radar/"
 
