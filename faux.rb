42a184e7542ba020fbaa6ace27f0e7eb9ba1b482
Ryan Moran <rmoran@vmware.com>
2022-03-22 10:04:45 -0700

    Updates faux version to 0.21.0

--- faux.rb
+++ faux.rb
@@ -1,14 +1,14 @@
 class Faux < Formula
   desc "Golang fake generator"
   homepage "https://github.com/ryanmoran/faux"
-  version "v0.20.0"
+  version "v0.21.0"
 
   if OS.mac?
     url "https://github.com/ryanmoran/faux/releases/download/#{version}/faux-darwin-amd64"
-    sha256 "a451a1d411c9730904b6d9f71945a7592ee050ab2ae823533ec8d0100dfe4351"
+    sha256 "01257f83fdb01ecbb6ef1072319c54c1bb382bedb5bc510288772fffb655b307"
   elsif OS.linux?
     url "https://github.com/ryanmoran/faux/releases/download/#{version}/faux-linux-amd64"
-    sha256 "884c92fcc8c36f3c9524045da7d66fcb2892b3688c2fa703769a3eb1116f6452"
+    sha256 "96973345af38ed6d05734b9b44bb76a711310c564dced310134951b65a5bf2b8"
   end
 
   depends_on :arch => :x86_64
